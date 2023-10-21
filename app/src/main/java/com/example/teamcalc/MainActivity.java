c5b1b751c868e89a4d7e6633220c2eaa0ba7ae29
StergiosPar <stelios.parashos@gmail.com>
2023-10-21 17:38:49 +0300

    gieaaa

--- app/src/main/java/com/example/teamcalc/MainActivity.java
+++ app/src/main/java/com/example/teamcalc/MainActivity.java
@@ -13,6 +13,6 @@ public class MainActivity extends AppCompatActivity{
         ena dio tria
         rfehrthtr
                 aaaaaaaaaaaaaaaaaaaaaaaaaa
-                         eimai o stelios test
+                         eimai o stelios testf
     }
 }
