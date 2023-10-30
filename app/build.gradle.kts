de40efed2bcd77e21e8f8532119e3790a85ed03e
AriZume <128248493+AriZume@users.noreply.github.com>
2023-10-30 16:03:47 +0200

    Code deletion

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,7 +32,6 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("io.apisense:rhino-android:1.2.0")
