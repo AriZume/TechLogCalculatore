7e759a1e28b9948ffc72b954f9f559cdce466b40
alk-an <147655333+alk-an@users.noreply.github.com>
2023-10-30 16:50:15 +0200

    7 8 9 kai epi

--- app/src/main/java/com/example/teamcalc/MainActivity.java
+++ app/src/main/java/com/example/teamcalc/MainActivity.java
@@ -9,4 +9,21 @@ public class MainActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void sevenOnClick(View view){
+        setEquation("7");
+    }
+
+    public void eightOnClick(View view){
+        setEquation("8");
+    }
+
+    public void nineOnClick(View view){
+        setEquation("9");
+    }
+
+    public void multOnClick(View view){
+        if(equation.length() > 0)
+            setEquation("*");
+    }
 }
