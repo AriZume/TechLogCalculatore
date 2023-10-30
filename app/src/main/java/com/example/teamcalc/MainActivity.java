e41d2563c7e4fa17c303b2362aa1dfdb6f64d481
kapadokos <kapadokos@yahoo.com>
2023-10-30 16:55:40 +0200

    line 5 mainactivity

--- app/src/main/java/com/example/teamcalc/MainActivity.java
+++ app/src/main/java/com/example/teamcalc/MainActivity.java
@@ -8,5 +8,23 @@ public class MainActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
+    public void oneOnClick(View view){
+        setEquation("1");
+    }
+
+    public void twoOnClick(View view){
+        setEquation("2");
+    }
+
+    public void threeOnClick(View view){
+        setEquation("3");
+    }
+    public void plusOnClick(View view){
+        if(equation.length() > 0)
+            setEquation("+");
+    }
+
 }
