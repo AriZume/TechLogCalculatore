7d927fbfabd074001ff9fb302fa657c9fc07630e
StergiosPar <stelios.parashos@gmail.com>
2023-10-30 16:40:10 +0200

    Revert "Row 1st"
    This reverts commit 1d346554759c0e8c692695a0c8093f5d0e602992.

--- app/src/main/java/com/example/teamcalc/MainActivity.java
+++ app/src/main/java/com/example/teamcalc/MainActivity.java
@@ -1,8 +1,6 @@
 package com.example.teamcalc;
 
 import android.os.Bundle;
-import android.view.View;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity{
@@ -10,45 +8,5 @@ public class MainActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    private void changeSqrt(Integer index){
-        String number = "";
-
-        for(int i=index + 1; i<equation.length();i++){
-            if(isNumeric(equation.charAt(i)))
-                number = number + equation.charAt(i);
-            else
-                break;
-        }
-
-        String original = "√" + number;
-        String changed = "Math.sqrt(" + number + ")";
-        tempFormula = tempFormula.replace(original,changed);
-    }
-    public void clearOnClick(View view){
-        equation="";
-        tvEquation.setText("0");
-        tvResult.setText("");
-    }
-
-    public void delOnClick(View view){
-        if(equation!=null && (equation.length() > 0))
-            equation = equation.substring(0, equation.length() - 1);
-        tvEquation.setText(equation);
-    }
-
-    public void powerOfOnClick(View view) {
-        if(equation.length() > 0)
-            setEquation("^");
-    }
-
-    public void sqrtOnClick(View view){
-        setEquation("√");
-    }
-
-    public void modOnClick(View view){
-        if(equation.length() > 0)
-            setEquation("%");
     }
 }
